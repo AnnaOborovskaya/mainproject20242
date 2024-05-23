aa9524dd5283cfb489b382e4160ba957fc383527
AnnaOborovskaya <118750918+AnnaOborovskaya@users.noreply.github.com>
2024-05-23 18:56:24 +0700

    Update db.py

--- db.py
+++ db.py
@@ -12,6 +12,7 @@ engine_s = create_async_engine(ur_a, echo=True)
 Base = declarative_base()
 SessionLocal = sessionmaker(engine_s, class_=AsyncSession, expire_on_commit=False)
 
+
 async def create_tables():
     async with engine_s.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+        pass
